b4223ed7d0fa2eba761105a5f2e098a66c6db358
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-05 02:19:53 +0100

    added development data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet excepturi velit unde itaque incidunt, molestiae illum debitis reprehenderit voluptas deleniti! Aliquam laborum quidem eos qui odio. Similique quas sit tempora.',
+    rating: 4,
+  },
+  {
+    id: 2,
+    review:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum necessitatibus pariatur aspernatur. Earum quos quas dolorum provident id, eum praesentium atque inventore, nisi officia pariatur enim delectus, velit corporis! Laudantium.',
+    rating: 5,
+  },
+  {
+    id: 3,
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, adipisci. Quasi dolorem, necessitatibus ipsum ut accusantium sint at nisi quis unde voluptas recusandae a temporibus esse hic facilis! Ipsam, corporis?',
+    rating: 3,
+  },
+  {
+    id: 4,
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis cupiditate non, qui a sunt praesentium deleniti debitis veniam eligendi possimus maxime reprehenderit quasi ipsa ratione. Saepe accusamus assumenda distinctio rem.',
+    rating: 2,
+  },
+  {
+    id: 5,
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit doloremque blanditiis rem natus qui? Possimus praesentium, facilis temporibus mollitia amet totam maiores, doloribus, ipsa quibusdam minus deleniti quos unde quas!',
+    rating: 1,
+  },
+];
