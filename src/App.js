93f41e5e079d79680ade5820a2b3f5393901ebd7
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-06 00:22:10 +0100

    refactored

--- src/App.js
+++ src/App.js
@@ -1,94 +1,17 @@
-import { AiFillStar } from 'react-icons/ai';
-import { useEffect, useRef, useState } from 'react';
-
 import Title from 'components/Title';
-import Button from 'components/Button';
 import Footer from 'components/Footer';
 import Reviews from 'components/Reviews';
-import { colors, data } from 'utils/data';
 import BackToTop from 'components/BackToTop';
-import { getFromStorage, setToStorage, tokenKey } from 'utils';
-
-const getLocalStorage = () => {
-  const reviews = getFromStorage(tokenKey);
-  return reviews ?? data;
-};
+import AddReview from 'components/AddReview';
+import { useGlobalContext } from 'context/ReviewContext';
 
 function App() {
-  const reviewRef = useRef();
-  const stars = Array(5).fill(0);
-  const [review, setReview] = useState('');
-  const [hoverValue, setHoverValue] = useState(null);
-  const [currentValue, setCurrentValue] = useState(0);
-  const [reviews, setReviews] = useState(getLocalStorage());
-
-  const handleClick = (value) => {
-    setCurrentValue(value);
-  };
-
-  const handleMouseOver = (value) => {
-    setHoverValue(value);
-  };
-
-  const handleMouseLeave = () => {
-    setHoverValue(null);
-  };
-
-  const handleSubmit = () => {
-    const newPost = {
-      id: new Date().getTime(),
-      rating: currentValue,
-      review,
-    };
-
-    setReviews((prev) => [newPost, ...prev]);
-    setReview('');
-    setCurrentValue(0);
-  };
-
-  useEffect(() => {
-    reviewRef.current.focus();
-    setToStorage(tokenKey, reviews);
-  }, [reviews]);
+  const { reviews } = useGlobalContext();
 
   return (
     <div className='container'>
       <Title />
-      <textarea
-        placeholder='What`s your feedback'
-        value={review}
-        ref={reviewRef}
-        onChange={(e) => setReview(e.target.value)}
-      />
-      <div className='reviewer-meta'>
-        <div className='reviewer-stars'>
-          {stars.map((_, index) => {
-            return (
-              <AiFillStar
-                key={index}
-                size={17}
-                onClick={() => handleClick(index + 1)}
-                color={
-                  (hoverValue || currentValue) > index
-                    ? colors.orange
-                    : colors.gray
-                }
-                onMouseOver={() => handleMouseOver(index + 1)}
-                onMouseLeave={handleMouseLeave}
-                className='icon'
-              />
-            );
-          })}
-        </div>
-        <div className='btn-wrapper'>
-          <Button
-            text='Submit review →'
-            onClick={handleSubmit}
-            disabled={review === '' ?? false}
-          />
-        </div>
-      </div>
-
+      <AddReview />
       <Reviews reviews={reviews} />
       <Footer />
       <BackToTop />
