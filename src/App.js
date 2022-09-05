4c000d7afe0e1525233a4f9fff24552de4818e65
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-05 16:22:08 +0100

    refactored and added new functionalities

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { useState } from 'react';
 import { AiFillStar } from 'react-icons/ai';
+import { useEffect, useRef, useState } from 'react';
 
-import './App.css';
+import Title from 'components/Title';
+import Button from 'components/Button';
+import Footer from 'components/Footer';
+import Reviews from 'components/Reviews';
+import { colors, data } from 'utils/data';
+import BackToTop from 'components/BackToTop';
+import { getFromStorage, setToStorage, tokenKey } from 'utils';
 
-const colors = {
-  orange: '#FFBA5A',
-  gray: '#a9a9a9',
+const getLocalStorage = () => {
+  const reviews = getFromStorage(tokenKey);
+  return reviews ?? data;
 };
 
 function App() {
+  const reviewRef = useRef();
   const stars = Array(5).fill(0);
+  const [review, setReview] = useState('');
   const [hoverValue, setHoverValue] = useState(null);
   const [currentValue, setCurrentValue] = useState(0);
+  const [reviews, setReviews] = useState(getLocalStorage());
 
   const handleClick = (value) => {
     setCurrentValue(value);
@@ -25,54 +34,66 @@ function App() {
     setHoverValue(null);
   };
 
+  const handleSubmit = () => {
+    const newPost = {
+      id: new Date().getTime(),
+      rating: currentValue,
+      review,
+    };
+
+    setReviews((prev) => [newPost, ...prev]);
+    setReview('');
+    setCurrentValue(0);
+  };
+
+  useEffect(() => {
+    reviewRef.current.focus();
+    setToStorage(tokenKey, reviews);
+  }, [reviews]);
+
   return (
-    <div style={styles.container}>
-      <h2>Star Ratings in React</h2>
-      <div>
-        {stars.map((_, index) => {
-          return (
-            <AiFillStar
-              key={index}
-              size={24}
-              onClick={() => handleClick(index + 1)}
-              style={{ marginRight: 10, cursor: 'pointer' }}
-              color={(hoverValue || currentValue) > index ? colors.orange : colors.gray}
-              onMouseOver={() => handleMouseOver(index + 1)}
-              onMouseLeave={handleMouseLeave}
-            />
-          );
-        })}
-      </div>
+    <div className='container'>
+      <Title />
       <textarea
         placeholder='What`s your feedback'
-        style={styles.textarea}
+        value={review}
+        ref={reviewRef}
+        onChange={(e) => setReview(e.target.value)}
       />
-      <button style={styles.button}>Submit</button>
+      <div className='reviewer-meta'>
+        <div className='reviewer-stars'>
+          {stars.map((_, index) => {
+            return (
+              <AiFillStar
+                key={index}
+                size={17}
+                onClick={() => handleClick(index + 1)}
+                color={
+                  (hoverValue || currentValue) > index
+                    ? colors.orange
+                    : colors.gray
+                }
+                onMouseOver={() => handleMouseOver(index + 1)}
+                onMouseLeave={handleMouseLeave}
+                className='icon'
+              />
+            );
+          })}
+        </div>
+        <div className='btn-wrapper'>
+          <Button
+            text='Submit review →'
+            onClick={handleSubmit}
+            disabled={review === '' ?? false}
+          />
+        </div>
+      </div>
+
+      <Reviews reviews={reviews} />
+      <Footer />
+      <BackToTop />
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  textarea: {
-    border: '1px solid #a9a9a9',
-    margin: '20px 0',
-    borderRadius: 5,
-    resize: 'none',
-    minHeight: 100,
-    width: 300,
-    padding: 10,
-  },
-  button: {
-    border: '1px solid #a9a9a9',
-    borderRadius: 5,
-    width: 300,
-    padding: 10,
-  },
-};
+}
 
 export default App;
