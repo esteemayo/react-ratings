b67890c7ac93306d1824d2c50eebcc1746b85d67
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-05 16:21:25 +0100

    created reviews component

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import ReviewCard from './ReviewCard';
+
+const Reviews = ({ reviews }) => {
+  return (
+    <div className='wrapper'>
+      {reviews.map((item) => {
+        return <ReviewCard key={item.id} {...item} />;
+      })}
+    </div>
+  );
+};
+
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      review: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Reviews;
