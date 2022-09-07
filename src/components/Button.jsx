bddca2c2a6fed5ba494ee9a596599a081202a0e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-07 14:58:49 +0100

    added icon props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ text, ...rest }) => {
+const Button = ({ text, icon, ...rest }) => {
   return (
     <button {...rest} className='btn'>
-      {text}
+      {text} {icon}
     </button>
   );
 };
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  icon: PropTypes.object,
 };
 
 export default Button;
