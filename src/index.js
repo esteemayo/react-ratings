e25efc3b862d26f4d13a0914c2aad01d6a890f72
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-07 02:07:01 +0100

    added DarkMode ContextAPI Provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ReviewProvider from 'context/ReviewContext';
+import ReviewProvider from 'context/reviews/ReviewContext';
+import DarkModeProvider from 'context/darkmode/DarkModeContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReviewProvider>
-      <App />
-    </ReviewProvider>
+    <DarkModeProvider>
+      <ReviewProvider>
+        <App />
+      </ReviewProvider>
+    </DarkModeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
