7442cba6c01f51c778f2feff86b7e4f9fd6669f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-08 02:37:02 +0100

    added new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react';
 import { DarkModeOutlined, LightModeOutlined } from '@mui/icons-material';
+
 import { useDarkModeGlobalContext } from 'context/darkmode/DarkModeContext';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
   const { darkMode, toggle } = useDarkModeGlobalContext();
 
+  const toggleIsScrolled = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', toggleIsScrolled);
+    return () => document.removeEventListener('scroll', toggleIsScrolled);
+  }, []);
+
   return (
-    <nav className='navbar'>
+    <nav className={isScrolled ? 'navbar scrolled' : 'navbar'}>
       <div className='navbar__left'>
         <h1 className='logo'>Ratings&reg;</h1>
       </div>
